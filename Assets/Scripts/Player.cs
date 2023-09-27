532827fe2b8dff4e0298e8748e9645736048c614
rgo555 <95858482+Edgarduba@users.noreply.github.com>
2023-09-27 10:13:10 +0200

    Plataforma2D

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Stats")]
+    [Tooltip("Controla la velocidad d movimiento del personaje")]
+
     [SerializeField]private float _playerSpeed = 5;
     //los corchetes estos " [] " son para que se vea en el inspector
+    [Tooltip("Controla la fuerza de salto del personaje")]
     [SerializeField]private float _jumpForce = 5;
      
     private float _playerInput;
@@ -13,11 +17,13 @@ public class Player : MonoBehaviour
     private float _playerInputHorizontal;
     private Rigidbody2D _rBody2D;
     private GroundSensor _sensor;
+    private Animator _animator;
     
     void Start()
     {
         _rBody2D = GetComponent<Rigidbody2D>();
         _sensor = GetComponentInChildren<GroundSensor>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -35,26 +41,35 @@ public class Player : MonoBehaviour
     }
     
     void FixedUpdate() 
-        {
+    {
         //_rBody2D.AddForce(new Vector2(_playerInputHorizontal * _playerSpeed, 0), ForceMode2D.Impulse);
         //esta línea se utilizará para hacer el salto ;)
-
         _rBody2D.velocity = new Vector2(_playerInputHorizontal * _playerSpeed, _rBody2D.velocity.y);
-        }
+    }
     
-        void PlayerMovement()
-        {
+    void PlayerMovement()
+    {
         _playerInputHorizontal = Input.GetAxis("Horizontal");
         //_playerInputVertical = Input.GetAxis("Vertical");
         //transform.Translate(new Vector2(_playerInputHorizontal, _playerInputVertical) * _playerSpeed * Time.deltaTime);
+        
+        if(_playerInputHorizontal !=0)
+        {
+            _animator.SetBool("IsRunning", true);
         }
 
+        if(_playerInputHorizontal ==0)
+        {
+            _animator.SetBool("IsRunning", false);
+        }
+    }
+
     void Playerjump()
-       {
+    {
            _rBody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);  
         //if = condicionante
        //_rBody2D porque en los "()" indicas como hay 2 vertices zy y xy, y como solo afecta el jumpforce al xy
-       }
+    }
 
 }
 
