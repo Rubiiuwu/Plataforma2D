cff353b72e5783bbb4007fb06d5534c9cf908198
rgo555 <95858482+Edgarduba@users.noreply.github.com>
2023-10-04 11:56:46 +0200

    Plataforma2D

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Doc para almacenar datos :) no se puede añadir a ningún objeto
+public class Global
+{
+    public static int maxLevel;
+    public static int maxScores;
+    public static float playerHealth;
+    public static float playerSpeed;
+    public static float playerDamage;
+}
